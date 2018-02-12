ecbc78822330004be4447157b6fb429115c5c726
DESKTOP-LP1J4GQ\garci <garciademata@gmail.com>
2018-02-12 11:15:13 +0100

    truffle.js fixed for testing purpose, needs to be updated to include Alastria network issue #1

--- truffle.js
+++ truffle.js
@@ -3,12 +3,7 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
-    },
-     alastria-uport: { //aws for uport testing
-       host: "",
-       port: ,
-       network_id: "*"
-     }
+      network_id: "*" 
+    }
   }
 };
