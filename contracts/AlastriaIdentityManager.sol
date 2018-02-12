36ae239b5442dd430900205af86db70de1559369
DESKTOP-LP1J4GQ\garci <garciademata@gmail.com>
2018-02-12 11:11:29 +0100

    AlastriaIdentityManager and BasicIdentityManager updated, not compiling, requires registry interface

---
+++ contracts/AlastriaIdentityManager.sol
@@ -0,0 +1,102 @@
+pragma solidity ^0.4.8;
+import "./BasicIdentityManager.sol";
+
+
+contract AlastriaIdentityManager is BasicIdentityManager{
+
+    struct Attest {
+      bytes32 attHash;
+      bytes32 attUri;
+    }
+
+    mapping(bytes32 => uint) attestId;
+
+    event ev_newAtestaion(bytes32 _attHash, string _attUri);
+
+    Attest[] public Atestation;
+    uint public numAtestation;
+
+    modifier onlyOlderOwner(address identity, address sender) {
+        require(isOlderOwner(identity, sender));
+        _;
+    }
+
+    modifier onlyRecovery(address identity, address sender) {
+        require(recoveryKeys[identity] == sender);
+        _;
+    }
+
+    modifier rateLimited(Proxy identity, address sender) {
+        require(limiter[identity][sender] < (now - adminRate));
+        limiter[identity][sender] = now;
+        _;
+    }
+
+    /// @dev Allows an identity to linked attestations
+    /// @param _attHash Hash of the attestation
+    /// @param _attUri Uri allocating attestation
+    function newAtestation(bytes32 _attHash, string _attUri) public {
+      Atestation.push(Attest({
+        attHash: _attHash,
+        attUri: _attUri})
+      );
+      attestId[_attHash] = numAtestation;
+      numAtestation = numAtestation + 1;
+      ev_newAtestaion(_attHash, _attUri);
+    }
+
+    /// @dev Allows a recoveryKey to add a new owner with userTimeLock waiting time
+    function addOwnerFromRecovery(address sender, Proxy identity, address newOwner) public
+        onlyAuthorized
+        onlyRecovery(identity, sender)  //eIDAS
+        rateLimited(identity, sender)
+    {
+        require(!isOwner(identity, newOwner));
+        owners[identity][newOwner] = now;
+        LogOwnerAdded(identity, newOwner, sender);
+    }
+
+    /// @dev Allows an olderOwner to add a new owner instantly
+    function addOwner(address sender, Proxy identity, address newOwner) public
+        onlyAuthorized
+        onlyOlderOwner(identity, sender)
+        rateLimited(identity, sender)
+    {
+        require(!isOwner(identity, newOwner));
+        owners[identity][newOwner] = now - userTimeLock;
+        LogOwnerAdded(identity, newOwner, sender);
+    }
+
+    /// @dev Allows an owner to remove another owner instantly
+    function removeOwner(address sender, Proxy identity, address owner) public
+        onlyAuthorized
+        onlyOlderOwner(identity, sender)
+        rateLimited(identity, sender)
+    {
+        // an owner should not be allowed to remove itself
+        require(sender != owner);
+        delete owners[identity][owner];
+        LogOwnerRemoved(identity, owner, sender);
+    }
+
+    /// @dev Allows to recover attestations without burning gas
+    /// @param _id identification number for an attestation
+    function getAtestationById(uint _id) constant internal returns(Attest){
+      return Attest[_id];
+    }
+
+    /// @dev Allows to recover attestations without burning gas
+    /// @param _attHash hash index in mapping for attestation
+    function getAtestationByHash(bytes32 _attHash) constant internal returns (Attest){
+      return getAtestationById(attestId[_attHash]);
+    }
+
+    function isOlderOwner(address identity, address owner) public constant returns (bool) {
+        return (owners[identity][owner] > 0 && (owners[identity][owner] + adminTimeLock) <= now);
+    }
+
+    function isRecovery(address recoveryKey) public constant returns (bool) {
+        return idasLvl[recoveryKey] == 3;
+    }
+
+}
---
+++ contracts/BasicIdentityManager.sol
@@ -0,0 +1,76 @@
+pragma solidity 0.4.15;
+import "./Proxy.sol";
+
+
+contract BasicIdentityManager {
+    uint adminTimeLock;
+    uint userTimeLock;
+    uint adminRate;
+
+    event LogIdentityCreated(
+        address indexed identity,
+        address indexed creator,
+        address owner
+        );
+
+    mapping(address => mapping(address => uint)) owners;
+
+    ///TxRelay linked to realay || checkMessageData(msg.sender)
+    modifier onlyAuthorized() {
+        require(msg.sender == checkMessageData(msg.sender));
+        _;
+    }
+
+    modifier onlyOwner(address identity, address sender) {
+        require(isOwner(identity, sender));
+        _;
+    }
+
+    modifier validAddress(address addr) { //protects against some weird attacks
+        require(addr != address(0));
+        _;
+    }
+
+    /// @dev Contract constructor sets initial timelocks and meta-tx relay address
+    /// @param _userTimeLock Time before new owner added by recovery can control proxy
+    /// @param _adminTimeLock Time before new owner can add/remove owners
+    /// @param _adminRate Time period used for rate limiting a given key for admin functionality
+    /// @param _relayAddress Address of meta transaction relay contract remove in first implementation
+    function BasicIdentityManager(uint _userTimeLock, uint _adminTimeLock, uint _adminRate) {
+        require(_adminTimeLock >= _userTimeLock);
+        adminTimeLock = _adminTimeLock;
+        userTimeLock = _userTimeLock;
+        adminRate = _adminRate;
+    }
+
+    /// @dev Creates a new proxy contract for an owner and recovery
+    /// @param owner Key who can use this contract to control proxy. Given full power
+    /// Gas cost of ~300,000
+    function createIdentity(address owner) public{
+        Proxy identity = new Proxy();
+        owners[identity][owner] = now - adminTimeLock; // This is to ensure original owner has full power from day one
+        LogIdentityCreated(identity, msg.sender, owner);
+    }
+
+    /// @dev Allows a user to forward a call through their proxy.
+    function forwardTo(address sender, Proxy identity, address destination, uint value, bytes data) public
+        onlyAuthorized
+        onlyOwner(identity, sender)
+    {
+        identity.forward(destination, value, data);
+    }
+
+        //Checks that address a is the first input in msg.data.
+    //Has very minimal gas overhead.
+    function checkMessageData(address a) internal constant returns (bool t) {
+        if (msg.data.length < 36) return false;
+        assembly {
+            let mask := 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+            t := eq(a, and(mask, calldataload(4)))
+        }
+    }
+
+    function isOwner(address identity, address owner) public constant returns (bool) {
+        return (owners[identity][owner] > 0 && (owners[identity][owner] + userTimeLock) <= now);
+    }
+}
