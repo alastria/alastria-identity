952f59076c4d48c1b1e70a6b7a32b39096aed7e3
carlospastormatut <33754475+carlospastormatut@users.noreply.github.com>
2018-04-30 16:14:51 +0200

    Update AlastriaAttestationRegistry.sol

--- contracts/AlastriaAttestationRegistry.sol
+++ contracts/AlastriaAttestationRegistry.sol
@@ -1,7 +1,9 @@
-pragma solidity ^0.4.8;
+pragma solidity ^0.4.20;
 
 contract AlastriaAttestationRegistry{
   //Variables
+
+  // ¿Should we add Level of Assurance? 
   struct attestation {
     bool exists;
     bytes32 status;
@@ -19,26 +21,26 @@ contract AlastriaAttestationRegistry{
       _;
   }
 
-  modifier onlyAllowed (address subject, bytes32 rKey) {
-    require(msg.sender == subject || revocationChallenge()); //Cortocircuito?? ejecuta revocationChallenge inlcuso con issuer ok
+  modifier onlyAllowed (address subject, bytes32 dataHash, bytes32 rKey) {
+    require(msg.sender == subject || revocationChallenge(subject, dataHash, rKey)); //Cortocircuito?? ejecuta revocationChallenge inlcuso con issuer ok
     _;
   }
 
   //Functions
-  function set(bytes32 dataHash, bytes32 revocationChallenge, string URI) {
+  function set(bytes32 dataHash, bytes32 revocationChallenge, string URI) public {
     require(!registry[msg.sender][dataHash].exists);
-    var value = attestation(true, "Valid", revocationChallenge, URI);
-    registry[msg.sender][dataHash] = value;
+    registry[msg.sender][dataHash] = attestation(true, "Valid", revocationChallenge, URI);
   }
 
   //If the attestation does not exists the return is a void attestation
   //If we want a log, should we add an event?
-  function get(bytes32 dataHash, address subject) constant public returns (attestation) {
-    return registry[subject][dataHash];
+  function get(bytes32 dataHash, address subject) constant public returns (bool exists, bytes32 status) {
+    attestation storage value = registry[subject][dataHash];
+    return (value.exists, value.status);
   }
 
-  function revoke(address subject, bytes32 dataHash, bytes32 status, bytes32 rKey) onlyAllowed(subject, rKey) {
-    var value = registry[subject][dataHash];
+  function revoke(address subject, bytes32 dataHash, bytes32 status, bytes32 rKey) public onlyAllowed(subject, dataHash, rKey) {
+    attestation storage value = registry[subject][dataHash];
     //Need verification and message standarization
     if(status == "Revoked") {
       value.status = "Revoked";
@@ -51,8 +53,8 @@ contract AlastriaAttestationRegistry{
     }
   }
 
-  //Mocked implemantation with fixed value
-  function revocationChallenge() returns (bool) {
-    return true;
+  // Simple implementation
+  function revocationChallenge(address subject, bytes32 dataHash, bytes32 rKey) public view returns (bool) {
+    return keccak256 (dataHash, rKey) == registry[subject][dataHash].revocationChallenge;
   }
 }
