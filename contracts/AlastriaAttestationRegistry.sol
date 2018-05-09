111c8a97b8305abc884c0e74794fc14bbb2e03e0
carlospastormatut <33754475+carlospastormatut@users.noreply.github.com>
2018-05-09 17:12:23 +0200

    Update AlastriaAttestationRegistry.sol

--- contracts/AlastriaAttestationRegistry.sol
+++ contracts/AlastriaAttestationRegistry.sol
@@ -1,7 +1,10 @@
-pragma solidity ^0.4.20;
+pragma solidity ^0.4.19;
 
 contract AlastriaAttestationRegistry{
   //Variables
+  
+  int public version;	
+  address public previousPublishedVersion;
 
   enum Status {Valid, AskIssuer, Revoked, DeletedByUser} // Initially Valid, no backwards transitions allowed.
   struct attestation {
@@ -14,6 +17,14 @@ contract AlastriaAttestationRegistry{
   mapping (address => mapping(bytes32 => attestation)) private registry;
 
   //Events
+   event RevocationChallenge(
+    address indexed subject,
+    bytes32 indexed dataHash,
+	bytes32 revocationKey,
+ 	bytes32 revocationChallenge,
+ 	bytes32 calculatedRevocationChallenge
+	);
+
 
   //Modifiers
   modifier validAddress(address addr) { //protects against some weird attacks
@@ -28,6 +39,11 @@ contract AlastriaAttestationRegistry{
   }
 
   //Functions
+    function AlastriaAttestationRegistry (address _previousPublishedVersion) public {
+    version = 3;
+    previousPublishedVersion = _previousPublishedVersion;
+    }
+
   function set(bytes32 dataHash, bytes32 revocationChallenge, string URI) public {
     require(!registry[msg.sender][dataHash].exists);
     registry[msg.sender][dataHash] = attestation(true, Status.Valid, revocationChallenge, URI);
@@ -35,18 +51,18 @@ contract AlastriaAttestationRegistry{
 
   //If the attestation does not exists the return is a void attestation
   //If we want a log, should we add an event?
-  function get(bytes32 dataHash, address subject) constant public returns (bool exists, Status status) {
+  function get(address subject, bytes32 dataHash) constant public returns (bool exists, Status status) {
     attestation storage value = registry[subject][dataHash];
     return (value.exists, value.status);
   }
 
   function revoke(address subject, bytes32 dataHash, Status status, bytes32 rKey) public onlyAllowed(subject, dataHash, rKey) {
     attestation storage value = registry[subject][dataHash];
-
+	
     //Need verification
     if (status <= value.status) {
         // Do nothing, no backwards transitions allowed
-    } else if (value.status == Status.DeletedByUser && msg.sender == subject) {
+    } else if (status == Status.DeletedByUser && msg.sender == subject) {
       value.status = status;
     } else if(status == Status.Revoked) {
         value.status = status;
@@ -57,6 +73,19 @@ contract AlastriaAttestationRegistry{
 
   // Simple implementation
   function revocationChallenge(address subject, bytes32 dataHash, bytes32 rKey) public view returns (bool) {
-    return keccak256 (dataHash, rKey) == registry[subject][dataHash].revocationChallenge;
+    bool value;
+    bytes32 rc = registry[subject][dataHash].revocationChallenge;
+    bytes32 rcec = keccak256 (dataHash, rKey);
+    // value = keccak256 (dataHash, rKey) == registry[subject][dataHash].revocationChallenge;
+    value = rcec == rc ;
+	RevocationChallenge (subject, dataHash, rKey, rc, rcec);
+    return value;
+  }
+  
+  // Utility function 
+  function revocationHash(bytes32 dataHash, bytes32 rKey) public pure returns (bytes32) {
+	bytes32 value;
+	value = keccak256 (dataHash, rKey);
+	return value;
   }
 }
