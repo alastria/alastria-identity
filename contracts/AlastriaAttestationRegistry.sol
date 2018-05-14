83919d5d026780f5e6b5d3370dcfa6bc9ead6bfa
carlospastormatut <33754475+carlospastormatut@users.noreply.github.com>
2018-05-14 14:48:44 +0200

    Add files via upload

--- contracts/AlastriaAttestationRegistry.sol
+++ contracts/AlastriaAttestationRegistry.sol
@@ -1,20 +1,30 @@
 pragma solidity ^0.4.19;
 
+// Attestation are registered under Hash(Attestation) Revocations under Hash (Attestations + AttestationSignature)
 contract AlastriaAttestationRegistry{
   //Variables
   
-  int public version;	
+  int public version; 
   address public previousPublishedVersion;
 
-  enum Status {Valid, AskIssuer, Revoked, DeletedByUser} // Initially Valid, no backwards transitions allowed.
-  struct attestation {
+  // Attestation: Initially Valid: Only DeletedByUser
+  // Revocations: Initially Valid: Only AskIssuer or Revoked, no backwards transitions.
+  enum Status {Valid, AskIssuer, Revoked, DeletedByUser}
+  struct Attestation {
     bool exists;
     Status status;
-    bytes32 revocationChallenge;
     string URI;
   }
+  // Mapping subject, hash (JSON attestation)
+  mapping (address => mapping(bytes32 => Attestation)) private attestationRegistry;
 
-  mapping (address => mapping(bytes32 => attestation)) private registry;
+  struct Revocation {
+    bool exists;
+    Status status;
+  }
+  // Mapping issuer, hash (JSON attestation + AttestationSignature)
+  mapping (address => mapping(bytes32 => Revocation)) private revocationRegistry;
+
 
   //Events
 
@@ -24,52 +34,65 @@ contract AlastriaAttestationRegistry{
       _;
   }
 
-  modifier onlyAllowed (address subject, bytes32 dataHash, bytes32 rKey) {
-    // Only Subject or Issuer (Revocation Key Owner)
-    require(msg.sender == subject || revocationChallenge(subject, dataHash, rKey));
-    _;
-  }
-
   //Functions
-    function AlastriaAttestationRegistry (address _previousPublishedVersion) public {
+  function AlastriaAttestationRegistry (address _previousPublishedVersion) public {
     version = 3;
     previousPublishedVersion = _previousPublishedVersion;
-    }
+  }
 
-  function set(bytes32 dataHash, bytes32 revocationChallenge, string URI) public {
-    require(!registry[msg.sender][dataHash].exists);
-    registry[msg.sender][dataHash] = attestation(true, Status.Valid, revocationChallenge, URI);
+  function set(bytes32 dataHash, string URI) public {
+    require(!attestationRegistry[msg.sender][dataHash].exists);
+    attestationRegistry[msg.sender][dataHash] = Attestation(true, Status.Valid, URI);
+  }
+
+  function deleteAttestation (bytes32 dataHash) public {
+    Attestation storage value = attestationRegistry[msg.sender][dataHash];
+    // only existent
+    if (value.exists) {
+        value.status = Status.DeletedByUser;
+    }
   }
 
   //If the attestation does not exists the return is a void attestation
   //If we want a log, should we add an event?
-  function get(address subject, bytes32 dataHash) constant public returns (bool exists, Status status) {
-    attestation storage value = registry[subject][dataHash];
+  function subjectAttestationStatus (address subject, bytes32 dataHash) constant public returns (bool exists, Status status) {
+    Attestation storage value = attestationRegistry[subject][dataHash];
     return (value.exists, value.status);
   }
 
-  function revoke(address subject, bytes32 dataHash, Status status, bytes32 rKey) public onlyAllowed(subject, dataHash, rKey) {
-    attestation storage value = registry[subject][dataHash];
-	
-    //Need verification
-    if (status <= value.status) {
-        // Do nothing, no backwards transitions allowed
-    } else if (status == Status.DeletedByUser && msg.sender == subject) {
-      value.status = status;
-    } else if(status == Status.Revoked) {
-        value.status = status;
-    } else if(status == Status.AskIssuer && msg.sender != subject) {
-        value.status = status;
+  function revokeAttestation(bytes32 revHash, Status status) public {
+    Revocation storage value = revocationRegistry[msg.sender][revHash];
+    //No backward transition, only AskIssuer or Revoked
+    if (status > value.status) {
+        if (status == Status.AskIssuer || status == Status.Revoked) {
+            value.exists = true;
+            value.status = status;
+        }
     }
   }
 
-  // Simple implementation
-  function revocationChallenge(address subject, bytes32 dataHash, bytes32 rKey) public view returns (bool) {
-    return keccak256 (dataHash, rKey) == registry[subject][dataHash].revocationChallenge;
+  //If the attestation does not exists the return is a void attestation
+  //If we want a log, should we add an event?
+  function issuerRevocationStatus(address issuer, bytes32 revHash) constant public returns (bool exists, Status status) {
+    Revocation storage value = revocationRegistry[issuer][revHash];
+    return (value.exists, value.status);
   }
-  
-  // Utility function 
-  function revocationHash(bytes32 dataHash, bytes32 rKey) public pure returns (bytes32) {
-	return keccak256 (dataHash, rKey);
+
+  // Utility functions
+  // Defining three status functions avoid linking the subject to the issuer or the corresponding hashes
+  function attestationStatus (Status userStatus, Status issuerStatus) pure public returns (Status) {
+     if (userStatus >= issuerStatus) {
+        return userStatus;
+     } else {
+        return issuerStatus;
+     }
+  } 
+
+  function solidityHash(bytes data) public pure returns (bytes32) {
+	return keccak256 (data);
+  }
+
+  function solidityHash(bytes32 data1, bytes32 data2) public pure returns (bytes32) {
+	return keccak256 (data1, data2);
   }
 }
