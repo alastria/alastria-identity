e68f1e33188199ecde2aa4cefa5d91a64ef14881
Marcos Serradilla <marcos@io.builders>
2018-07-20 09:16:42 +0200

    Code quality integration.

--- truffle.js
+++ truffle.js
@@ -4,7 +4,7 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*"
-    },
+    }, // FIXME: This is a deprecated network, use alastria
     'alastria.uport': {
       host: "35.176.19.89",
       port: 22000,
@@ -12,7 +12,7 @@ module.exports = {
       gas: 0xffffff,
       gasPrice: 0x0,
       from: "0xd65616c46a2e55957aff33e238b31bc568358e20"
-    },
+    }, // FIXME: This is a deprecated network, use alastria
     'alastria.identity': {
       host: "35.176.19.89",
       port: 22000,
@@ -20,6 +20,14 @@ module.exports = {
       gas: 0xffffff,
       gasPrice: 0x0,
       from: "0xd65616c46a2e55957aff33e238b31bc568358e20"
-      }
+      }, 
+      'alastria': {
+        host: "35.176.19.89",
+        port: 22000,
+        network_id: "*",
+        gas: 0xffffff,
+        gasPrice: 0x0,
+        from: "0xd65616c46a2e55957aff33e238b31bc568358e20"
+      },
   }
 };
