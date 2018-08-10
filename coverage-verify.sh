b4935c44838b1cf6a16311d4569d251119a2b681
jimenezjj <jaime.jimenez@adhara.io>
2018-08-10 14:42:33 +0200

    fix coverage-verify script

--- coverage-verify.sh
+++ coverage-verify.sh
@@ -8,7 +8,7 @@ LIMITE=$1
 
 ./node_modules/.bin/solidity-coverage > coverage.log ; echo $? > result.log
 
-if [[ "$(cat result.log)" == "0" ]]; then 
+if [ "$(cat result.log)" = "0" ]; then 
     cat coverage.log | gawk -v limite="$LIMITE" -F"|" '$1~/All files +/ { 
         if ( ($2*1)<limite ) { 
             system("echo 1 > result.log");
