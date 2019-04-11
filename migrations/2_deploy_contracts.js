6a1e98959915cad9d8412da1381d8fb128e5fa17
msalitu <mariasalgadoitu@gmail.com>
2019-04-11 17:56:35 +0200

    1. Compiler version: 0.4.23  2.Naming update. Claim has been replaced by Presentation.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,10 @@
 var AlastriaPublicKeyRegistry = artifacts.require('./AlastriaPublicKeyRegistry.sol');
 var AlastriaAttestationRegistry = artifacts.require('./AlastriaAttestationRegistry.sol');
-var AlastriaClaimRegistry = artifacts.require('./AlastriaClaimRegistry.sol');
+var AlastriaPresentationRegistry = artifacts.require('./AlastriaPresentationRegistry.sol');
 
 
 module.exports = function(deployer, network, accounts) {
   deployer.deploy(AlastriaPublicKeyRegistry, accounts[0]);
   deployer.deploy(AlastriaAttestationRegistry, accounts[0]);
-  deployer.deploy(AlastriaClaimRegistry, accounts[0]);
+  deployer.deploy(AlastriaPresentationRegistry, accounts[0]);
 };
--- truffle.js
+++ truffle.js
@@ -13,5 +13,10 @@ module.exports = {
       gasPrice: 0x0,
       from: "0xd65616c46a2e55957aff33e238b31bc568358e20"
     },
+    compilers: {
+      solc: {
+        version: "^0.4.23", // A version or constraint - Ex. "^0.5.0"
+      }
+    }
   }
 };
