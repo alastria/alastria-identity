38398f7790082d472ac0fe3ff98aac2c1d6d34d1
nefera606 <garciademata@gmail.com>
2019-08-22 16:44:44 +0200

    Update proxy for being lighter

--- contracts/identityManager/AlastriaProxy.sol
+++ contracts/identityManager/AlastriaProxy.sol
@@ -4,36 +4,25 @@ import "../libs/Owned.sol";
 
 contract AlastriaProxy is Owned {
     address public owner;
-    address public recover;
 
     event Forwarded (address indexed destination, uint value, bytes data);
-    event Received (address indexed sender, uint value);
 
     modifier onlyOwner() {
         require(isOwner(msg.sender));
         _;
     }
 
-    modifier onlyOwnerOrRecover() {
-        require(isOwner(msg.sender)||isRecover(msg.sender));
-        _;
-    }
-
+    //TODO: upgradeable owner for version in Identity Manager
     constructor () public {
         owner = msg.sender;
-        recover = msg.sender;
     }
 
     function () payable public{
-        emit Received(msg.sender, msg.value);
+        revert();
     }
 
     function forward(address destination, uint value, bytes data) public onlyOwner {
         require(destination.call.value(value)(data));
         emit Forwarded(destination, value, data);
-    }
-
-    function isRecover(address addr) public view returns(bool) {
-        return addr == recover;
     }
 }
