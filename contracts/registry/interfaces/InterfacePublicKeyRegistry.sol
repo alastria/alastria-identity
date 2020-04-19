b215cce862c5746adcc0b06a4712a3a3f93b9d48
IGM <garciademata@gmail.com>
2020-04-19 14:47:25 +0200

    Fix interface import

--- contracts/registry/interfaces/InterfacePublicKeyRegistry.sol
+++ contracts/registry/interfaces/InterfacePublicKeyRegistry.sol
@@ -1,24 +1,34 @@
 pragma solidity 0.6.4;
 pragma experimental ABIEncoderV2;
 
+enum Status {Valid, RevokedBySubject}
+
+struct PublicKey {
+    bool exists;
+    Status status; // Deleted keys shouldnt be used, not even to check previous signatures.
+    uint startDate;
+    uint endDate;
+    string key;
+    string algorithm;
+}
+    
+interface InterfacePublicKeyRegistryStorage_v0 {  
+    // Initially Valid: could only be changed to DeletedBySubject for the time being.
+
+    function setPublicKeyRegistry(address subject, bytes32 keyIndex, PublicKey calldata key) external;
+
+    function getPublicKeyRegistry(address subject, bytes32 keyIndex) external view returns(PublicKey memory); 
+
+    function getPublicKeyRegistryList(address subject, uint256 index) external view returns(bytes32);
+
+    function lentghPublicKeyList(address subject) external view returns(uint256);
+}
+
 interface InterfacePublicKeyRegistry_v0 {
-    
-    enum Status {Valid, RevokedBySubject}
-    
-    struct PublicKey {
-        bool exists;
-        Status status; // Deleted keys shouldnt be used, not even to check previous signatures.
-        uint startDate;
-        uint endDate;
-        string key;
-        string algorithm;
-    }
     
     function getCurrentPublicKey(address subject) view external returns (string memory, string memory);
 
-    function getKeyHash(string calldata inputKey) external returns(bytes32);
-    
-    function getPublicKeyStatus(address subject, bytes32 publicKeyId) view external;
+    function getPublicKeyStatus(address subject, bytes32 publicKeyId) view external returns(bool exists, Status status, uint startDate, uint endDate);
     
     function addKey(string calldata publicKey, string calldata algorithm) external;
 
