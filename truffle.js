1b91cf5c28d62d30f416c4ddb4c876020eb4ca40
Qijiong Jiang <qjiang7@dxc.com>
2020-04-21 17:15:18 +0200

    fix truffle compile to get correct solc version

--- truffle.js
+++ truffle.js
@@ -13,10 +13,11 @@ module.exports = {
       gasPrice: 0x0,
       from: "0xd65616c46a2e55957aff33e238b31bc568358e20"
     },
-    compilers: {
-      solc: {
-        version: "^0.4.23", // A version or constraint - Ex. "^0.5.0"
-      }
+
+  },
+  compilers: {
+    solc: {
+      version: "0.4.23", // A version or constraint - Ex. "^0.5.0"
     }
   }
 
