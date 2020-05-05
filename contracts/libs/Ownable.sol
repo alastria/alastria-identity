538339425411dc1460c1cd5240fb080228d689da
miguel.gomez <miguel.gomez@izertis.com>
2020-05-05 08:51:34 +0200

    Added Initializable library and adapted Ownable and Context using upgradeable version "openzeppelin-contracts-ethereum-package"

--- contracts/libs/Ownable.sol
+++ contracts/libs/Ownable.sol
@@ -1,5 +1,7 @@
 pragma solidity 0.6.4;
 
+import "./Initializable.sol";
+
 import "./Context.sol";
 
 /**
@@ -7,15 +9,11 @@ import "./Context.sol";
  * there is an account (an owner) that can be granted exclusive access to
  * specific functions.
  *
- * By default, the owner account will be the one that deploys the contract. This
- * can later be changed with {transferOwnership}.
- *
  * This module is used through inheritance. It will make available the modifier
- * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * `onlyOwner`, which can be aplied to your functions to restrict their use to
  * the owner.
  */
- 
-contract Ownable is Context {
+contract Ownable is Initializable, Context {
     address private _owner;
 
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
@@ -23,10 +21,9 @@ contract Ownable is Context {
     /**
      * @dev Initializes the contract setting the deployer as the initial owner.
      */
-    constructor () internal {
-        address msgSender = _msgSender();
-        _owner = msgSender;
-        emit OwnershipTransferred(address(0), msgSender);
+    function initialize(address sender) public initializer {
+        _owner = sender;
+        emit OwnershipTransferred(address(0), _owner);
     }
 
     /**
@@ -40,15 +37,22 @@ contract Ownable is Context {
      * @dev Throws if called by any account other than the owner.
      */
     modifier onlyOwner() {
-        require(_owner == _msgSender(), "Ownable: caller is not the owner");
+        require(isOwner(), "Ownable: caller is not the owner");
         _;
+    }
+
+    /**
+     * @dev Returns true if the caller is the current owner.
+     */
+    function isOwner() public view returns (bool) {
+        return _msgSender() == _owner;
     }
 
     /**
      * @dev Leaves the contract without owner. It will not be possible to call
      * `onlyOwner` functions anymore. Can only be called by the current owner.
      *
-     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * > Note: Renouncing ownership will leave the contract without an owner,
      * thereby removing any functionality that is only available to the owner.
      */
     function renounceOwnership() public onlyOwner {
@@ -61,8 +65,17 @@ contract Ownable is Context {
      * Can only be called by the current owner.
      */
     function transferOwnership(address newOwner) public onlyOwner {
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     */
+    function _transferOwnership(address newOwner) internal {
         require(newOwner != address(0), "Ownable: new owner is the zero address");
         emit OwnershipTransferred(_owner, newOwner);
         _owner = newOwner;
     }
+
+    uint256[50] private ______gap;
 }
