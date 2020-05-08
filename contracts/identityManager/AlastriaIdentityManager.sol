e123d2abeca2106d1c2527057cdd4c120deef576
vicens.creus <vicens.creus@BL-VCREUS>
2020-05-08 19:33:21 +0200

    Function createAlastriaIdentity problems that we think that are derived the changes made in AlastriaProxy contract

--- contracts/identityManager/AlastriaIdentityManager.sol
+++ contracts/identityManager/AlastriaIdentityManager.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.4.23;
+pragma solidity 0.6.4;
 
 import "./AlastriaIdentityServiceProvider.sol";
 import "./AlastriaIdentityIssuer.sol";
@@ -16,7 +16,7 @@ contract AlastriaIdentityManager is AlastriaIdentityServiceProvider, AlastriaIde
     AlastriaCredentialRegistry public alastriaCredentialRegistry;
     AlastriaPresentationRegistry public alastriaPresentationRegistry;
     AlastriaPublicKeyRegistry public alastriaPublicKeyRegistry;
-    mapping(address => address) public identityKeys; //change to alastriaID created check bool
+    mapping(address => address payable) public identityKeys; //change to alastriaID created check bool
     mapping(address => uint) public pendingIDs;
 
     //Events
@@ -56,15 +56,15 @@ contract AlastriaIdentityManager is AlastriaIdentityServiceProvider, AlastriaIde
 
     /// @dev Creates a new AlastriaProxy contract for an owner and recovery and allows an initial forward call which would be to set the registry in our case
     /// @param addPublicKeyCallData of the call to addKey function in AlastriaPublicKeyRegistry from the new deployed AlastriaProxy contract
-    function createAlastriaIdentity(bytes addPublicKeyCallData) public validAddress(msg.sender) isPendingAndOnTime(msg.sender) {
+    function createAlastriaIdentity(bytes memory addPublicKeyCallData) public validAddress(msg.sender) isPendingAndOnTime(msg.sender) {
         AlastriaProxy identity = new AlastriaProxy();
-        identityKeys[msg.sender] = identity;
+        identityKeys[msg.sender] = identity.owner;
         pendingIDs[msg.sender] = 0;
         identity.forward(address(alastriaPublicKeyRegistry), 0, addPublicKeyCallData);//must be alastria registry call
     }
 
     /// @dev This method send a transaction trough the proxy of the sender
-    function delegateCall(address _destination, uint256 _value, bytes _data) public {
+    function delegateCall(address _destination, uint256 _value, bytes memory _data) public {
         require(identityKeys[msg.sender]!=address(0));
         AlastriaProxy identity = AlastriaProxy(address(identityKeys[msg.sender]));
         identity.forward(_destination,_value,_data);
