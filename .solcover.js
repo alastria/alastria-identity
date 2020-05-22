f997a2deec6b2f92445b2ef943e1de2a45ab230c
Víctor Nieves Sánchez <vnievess@gmail.com>
2020-05-22 13:20:58 +0200

    removed everything

--- .solcover.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  port: 7555,
-  testCommand: 'sleep 5 && ../node_modules/.bin/truffle test --network coverage',
-  accounts: 25,
-  copyPackages: ['uport-identity'],
-  skipFiles: ['misc/Migrations.sol']
-};
