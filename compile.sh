532365b08b1c768a67bcd63bf14eb382f727f070
Víctor Nieves Sánchez <vnievess@gmail.com>
2020-05-23 19:40:17 +0200

    removed everything (#112)

--- compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf ./contracts/bin/
-rm -rf ./contracts/abi/
-./node_modules/solc/solcjs ./contracts/registry/AlastriaPresentationRegistry.sol ./contracts/registry/AlastriaCredentialRegistry.sol ./contracts/registry/AlastriaPublicKeyRegistry.sol ./contracts/misc/Migrations.sol ./contracts/identityManager/AlastriaIdentityServiceProvider.sol ./contracts/identityManager/AlastriaProxy.sol ./contracts/identityManager/AlastriaIdentityManager.sol ./contracts/identityManager/AlastriaIdentityIssuer.sol ./contracts/libs/Owned.sol ./contracts/libs/Eidas.sol --bin -o ./contracts/bin/ --optimize
-./node_modules/solc/solcjs ./contracts/registry/AlastriaPresentationRegistry.sol ./contracts/registry/AlastriaCredentialRegistry.sol ./contracts/registry/AlastriaPublicKeyRegistry.sol ./contracts/misc/Migrations.sol ./contracts/identityManager/AlastriaIdentityServiceProvider.sol ./contracts/identityManager/AlastriaProxy.sol ./contracts/identityManager/AlastriaIdentityManager.sol ./contracts/identityManager/AlastriaIdentityIssuer.sol ./contracts/libs/Owned.sol ./contracts/libs/Eidas.sol --abi -o ./contracts/abi/ --optimize
