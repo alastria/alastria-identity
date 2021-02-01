955d6b6462b1c41f3b0a535223376c7d64d13f43
Morales Diego, Carlos (ES - Salamanca) <cmoralesdiego@deloitte.es>
2021-02-01 09:47:49 +0100

    Adding comments

--- setAdminProxy.js
+++ setAdminProxy.js
@@ -29,9 +29,9 @@ module.exports = async function () {
     //addIdentityServiceProvider with the admin proxy address
     const tx4 = await imInstance.addIdentityServiceProvider("0x7F4fbC3f46d6ece0eC8D05B41EaC195862c946d0");
     console.log("tx4", tx4);
-    //addEntity
+    //addEntity with proxy address
     const tx5 = await imInstance.addEntity("0x7F4fbC3f46d6ece0eC8D05B41EaC195862c946d0", "Entidad1", "A-28964782", "www.entidad1.com/logo", "www.entidad1.com/AID", "www.entidad1.com/AOA", true);
-    console.log("tx5",tx5);  
+    console.log("tx5",tx5);
   } catch (error) {
     console.log(error);
   }
